a439e84e308ba34a7a92d7c5f6192eb47caf8d25
Gal <gallib2@gmail.com>
2019-12-03 19:09:22 +0200

    add sounds + start menu wip

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    bool toStartMove;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(toStartMove)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+            Debug.Log("transform.position: " + transform.position);
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        toStartMove = true;
+    }
+}
